9eb6d3f924f6d6351cf8525eaace8eb99f286c4f
Турисов Роман Александрович <turisov-ra@psyhosp.lan>
2016-10-17 11:43:39 +0400

    Л2.П2. Проверка введенных значений

---
+++ Primer2/Primer2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReadAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            if (int.TryParse(Console.ReadLine(), out A))
+            {
+                if (int.TryParse(Console.ReadLine(), out B))
+                {
+                    //максимальное значение
+                    int Max;
+                    //если больше A
+                    if (A >= B)
+                    {
+                        Max = A;
+                    }
+                    //инчае больеш B
+                    else
+                    {
+                        Max = B;
+                    };
+                    //вывод результата
+                    Console.WriteLine("Максимальное = " + Max);
+                }
+                else
+                {
+                    Console.WriteLine("Неверное знаечение! Нужно ввести целое число.");
+                };
+            }
+            else
+            {
+                Console.WriteLine("Неверное знаечение! Нужно ввести целое число.");
+            }
+            //завершение программы
+            Console.ReadKey();
+        }
+
+    }
+}
